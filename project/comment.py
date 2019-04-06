8ccb0b3d56fa9e39b88bebca5776cb1c32e25799
peytontx1 <ttxia@emory.edu>
2019-04-06 17:06:03 -0400

    new

--- project/comment.py
+++ project/comment.py
@@ -9,12 +9,18 @@ reddit = praw.Reddit(client_id='B7R_0Uz49aARVQ',
 
 
 submission = reddit.submission(url='https://www.reddit.com/r/movies/comments/b951kc/hi_im_trevor_stevens_director_of_the_film_rock/')
+
+
 submission.comments.replace_more(limit=None)
+print(submission.comments)
+
+
 def get_comments(reddit):
-    for submission in reddit.subreddit('oil').top('year',limit=10):
+    i = 0
+    for submission in reddit.subreddit('movies').top('year',limit=10):
+        submission.comments.replace_more(limit=0)
         for comment in submission.comments.list():
             print(comment.body)
-
 
 def main():
     '''The main function that calls the other functions.'''
--- project/topic.py
+++ project/topic.py
@@ -1,6 +1,8 @@
 import praw
 import time
 import csv
+import pandas as pd
+import datetime as dt
 
 
 
@@ -8,25 +10,54 @@ reddit = praw.Reddit(client_id='B7R_0Uz49aARVQ',
                      client_secret='Cm4xgbhflZN8HvoYFhJnQossRMc',
                      user_agent='subSentiment')
 
-def get_posts(reddit):
+subreddit = reddit.subreddit('movies')
+top_subreddit = subreddit.top()
+top_subreddit = subreddit.top(limit=1000)
 
-    with open(r"out/dataset.csv", "a") as outfile:
-        for submission in reddit.subreddit('movies').top('year', limit=10):
-            print(submission.title)
-            data = [
-                submission.title,
-            ]
-            writer = csv.writer(outfile)
-            writer.writerow(data)
-
-
-
-
-
-def main():
-    '''The main function that calls the other functions.'''
-    get_posts(reddit)
+topics_dict = { "title":[], \
+                "score":[], \
+                "id":[], \
+                "url":[], \
+                "comms_num": [], \
+                "created": [], \
+                "body":[], \
+                "comment": []}
 
 
-if __name__ == '__main__':
-    main()
+def getall(top_subreddit):
+    i = 0
+    for submission in top_subreddit:
+        time = dt.datetime.fromtimestamp(submission.created)
+        year = str(time).split('-')[0]
+        month = str(time).split('-')[1]
+        print(year)
+        print(month)
+        print(i)
+        if year == '2018' and (month == '04' or month == '05' or month == '06' or month == '07' or month == '08'):
+            a = ""
+            topics_dict["title"].append(submission.title)
+            topics_dict["score"].append(submission.score)
+            topics_dict["id"].append(submission.id)
+            topics_dict["url"].append(submission.url)
+            topics_dict["comms_num"].append(submission.num_comments)
+            topics_dict["created"].append(submission.created)
+            topics_dict["body"].append(submission.selftext)
+            print('a')
+            submission.comments.replace_more(limit=0)
+            for comment in submission.comments.list():
+                a += comment.body + '&&'
+                print('b')
+            topics_dict["comment"].append(a)
+        i += 1
+
+topics_data = pd.DataFrame(topics_dict)
+
+def get_date(created):
+    return dt.datetime.fromtimestamp(created)
+
+_timestamp = topics_data["created"].apply(get_date)
+
+topics_data = topics_data.assign(timestamp = _timestamp)
+
+topics_data[0:2]
+topics_data.loc[0]["comment"]
